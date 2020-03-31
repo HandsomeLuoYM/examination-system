d9ec0d43eebacdf98177c6d8765bf43db593da89
LuoYM-Warehouse <git2224357949@qq.com>
2020-03-31 22:10:01 +0800

    3/31 添加文件的导入功能

--- src/main/com/examination/dao/FileDao.java
+++ src/main/com/examination/dao/FileDao.java
@@ -13,14 +13,56 @@ public class FileDao {
 
     private static final String PATH = System.getProperty("user.dir");
 
-    public static boolean storageFile(List<StringBuilder> list, String fileName) throws IOException {
+    public static boolean storageFile(List<StringBuilder> list, String fileName)  {
         File file = new File(PATH + "\\" +fileName);
-        FileOutputStream fileOutputStream = new FileOutputStream(file,false);
-        String content = "";
-        for (int i =0 ;i<list.size();i++){
-            content = content + (i+1) + "、" + list.get(i).toString() + "\n";
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file,false);
+            String content = "";
+            for (int i =0 ;i<list.size();i++){
+                content = content + (i+1) + "、" + list.get(i).toString() + "\n";
+            }
+            fileOutputStream.write(content.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        fileOutputStream.write(content.getBytes());
         return true;
+    }
+
+    public static List<StringBuilder> readFile(File file) {
+        List<StringBuilder> list = new ArrayList<>();
+        FileInputStream fileInputStream = null;
+        BufferedReader bufferedReader = null;
+        if (!file.exists() || !file.getName().contains("txt")){
+            return null;
+        }
+        try {
+            fileInputStream = new FileInputStream(file);
+            bufferedReader = new BufferedReader(new InputStreamReader(fileInputStream));
+            String raw;
+            for (int i = 0; null != (raw = bufferedReader.readLine()); i++) {
+                if (raw.contains("、")) {
+                    list.add(new StringBuilder(raw.substring(raw.indexOf("、") + 1, raw.length() - 1)));
+                } else {
+                    list.add(new StringBuilder(raw));
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                fileInputStream.close();
+                bufferedReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
     }
 }
--- src/main/com/examination/test/Test.java
+++ src/main/com/examination/test/Test.java
@@ -1,7 +1,9 @@
 package main.com.examination.test;
 
+import main.com.examination.dao.FileDao;
 import main.com.examination.util.CreatUtil;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -12,7 +14,8 @@ public class Test {
 //		System.out.print("请输入生成式子的数目：");
 //		Scanner scanner = new Scanner(System.in);
 //		int num = scanner.nextInt();
-		c.formulaNum(10,10);
+//		c.formulaNum(10,10);
+		FileDao.readFile(new File("D:\\idea_maven\\examination-system\\Exercises.docx"));
 	}
 }
 
